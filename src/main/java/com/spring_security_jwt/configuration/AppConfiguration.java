ef902fa73717d12c20420601f92ec61eebc4a885
truongphan4898 <truongphan4898@gmail.com>
2024-05-31 11:10:17 +0700

    add

---
+++ src/main/java/com/spring_security_jwt/configuration/AppConfiguration.java
@@ -0,0 +1,12 @@
+package com.spring_security_jwt.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.spring_security_jwt")
+public class AppConfiguration implements WebMvcConfigurer {
+}
---
+++ src/main/java/com/spring_security_jwt/jwt/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.spring_security_jwt.jwt.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
